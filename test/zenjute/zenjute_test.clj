24431d82c5575d2d0592d6665e8d3d5af66c4179
Artem Apostatov <alexeevdev@yahoo.com>
2021-12-16 15:39:19 +0300

    ns change

--- test/zenjute/zenjute_test.clj
+++ test/zenjute/zenjute_test.clj
@@ -1,60 +1,66 @@
 (ns zenjute.zenjute-test
   (:require [clojure.test :refer :all]
             [zen.core :as zen]
-            [sci.core :as sci]))
+            [sci.core :as sci]
+            [clojure.string :as str]))
 
-(remove-ns 'zenjute.zenjute-test)
+;(remove-ns 'zenjute.zenjute-test)
 
 (def paths {:paths ["test/zenjute/resources"]})
 (def ctx (zen/new-context paths {:unsafe true}))
 
-;; (zen/read-ns ctx 'mapping)
-;; (zen/load-ns ctx 'mapping)
+(zen/load-ns ctx 'mapping-two)
 
 (zen/load-ns ctx {'ns 'mapping
+                  'naive-mapping {:zen/tags #{'zen/tag}}
 
- 'NaiveMapping
- {:zen/tags #{'zen/naive-mapping}
-  :zj/body "(fn [{:keys [value]}]
-             {:identifier value
-              :identifier_type
-              (if-let [code (and value (-> value :type :coding first :code))]
-                code
-                (and value \"UPIN\"))})"}})
+                  'NaiveMapping
+
+                  {:zen/tags #{'naive-mapping}
+                   :zj/body '(fn [v] {:id (-> v :k :j :l)})}})
+
+(->> (zen/get-symbol ctx 'mapping/NaiveMapping)
+     :zj/body)
+
+(->> (zen/get-tag ctx 'mapping/naive-mapping)
+     first
+     (zen/get-symbol ctx)
+     :zj/body)
 
 @ctx
 
-(zen/get-symbol ctx 'mapping/NaiveMapping)
-(zen/get-tag ctx 'mapping/naive-mapping)
-;; => :zen/load-failed
+;TODOLIST:
+; - load schema from file
+; - load body as list of symbols
+; - evaluate body via sci and wrap around data as arg
+
+(deftest sci-test
+  (testing "if args are not given - the default argument is all data"
+    (let [sci-fn (get-fn-by-tag 'our-mappin)
+          data {:currency "boo"}
+          result (apply-sci sci-fn data)]
+      (is (= "BOO" result)))))
+
+;   or particular keys
 
 
-(defmethod operation 'doc-xlsx
-  [op]
-  (fn [ctx]
-    (-> ctx
-      deref
-      :ns
-      (get 'data)
-      vals
-      (->> (mapv :docs)
-           (remove nil?)
-           first
-           (reduce (fn [acc {:keys [title data]}]
-                     (assoc acc title
-                            (->> data (mapv create-single-cell) excel/table-grid))) {}))
-      excel/quick-open!)))
 
-(defn evaluation
-  [ctx data-from-outside]
-  (z))
+; - make path function ^:path[:k1 :k2 :k3]
+;   that works with arbitrary nestity
 
-(defn coerce-zen-schema
-  [ctx]
-  (let [op (-> ctx deref :ns (get 'docs) (get 'Documentation) operation)]
-    (op ctx)))
+(deftest path-test
+  (is (= :boo (check-path {:transaction_date
+                           {:from
+                            {:city ^zj/path[:currentDate]}}}
+                          {:currentData :boo}))))
 
-(coerce-zen-schema ctx)
+(fn [x] {:t {:f {:c (:cur x)}}})
+
+(get-in {:k {:j [1 2 3]}} [:k :j 0])
+
+;; (zen/get-symbol ctx 'mapping/NaiveMapping
+;; (zen/read-ns ctx 'mapping)
+;; (zen/load-ns ctx 'mapping)
 
 (deftest a-test
   (testing "FIXME, I fail."
--- test/zenjute_test/zenjute_test.clj
+++
@@ -1,8 +0,0 @@
-(ns myname.myapp-test
-  (:require [clojure.test :refer :all]
-            [zen.core :as zen]
-            [sci.core :as sci]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
