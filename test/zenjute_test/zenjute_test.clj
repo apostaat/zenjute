68e9e404cc8b62f860c57b2f5fed7bb9379575be
Artem Apostatov <alexeevdev@yahoo.com>
2021-12-16 12:59:56 +0300

    initial commit

---
+++ test/zenjute_test/zenjute_test.clj
@@ -0,0 +1,8 @@
+(ns myname.myapp-test
+  (:require [clojure.test :refer :all]
+            [zen.core :as zen]
+            [sci.core :as sci]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
