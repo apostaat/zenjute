8de14337c29943dd50c38d184f20ec74bdbe92f0
VictorGus <retaow@gmail.com>
2021-12-18 00:17:15 +0300

    add support for json

--- src/zenjute/operation.clj
+++ src/zenjute/operation.clj
@@ -3,15 +3,24 @@
             [clojure.java.io :as io]
             [clojure.edn   :as edn]))
 
+(defn read-body [{:keys [body] :as req}]
+  (let [cnt (slurp body)]
+    (if (#{"application/json"} (get-in req [:headers "content-type"]))
+      (json/parse-string cnt true)
+      (edn/read-string cnt))))
+
 ;;TODO: eval mapping here.
 ;;NOTE: body is a string of text and it must be evaluated by reader
 (defn eval-mapping [{:keys [body] :as req}]
   ;;just a plug at the moment
   (try
     (if body
-      (let [cnt (slurp body)]
+      (let [{:keys [data code] :as body*} (read-body req)
+            data* (edn/read-string data)
+            code* (edn/read-string code)]
+        (println (type body*))
         {:status 200
-         :body (edn/read-string cnt)})
+         :body data})
       {:status 400
        :body (json/generate-string
               {:message "No body in the request"})})
@@ -22,3 +31,5 @@
   (let [static-page (-> "index.html" io/resource slurp)]
     {:status 200
      :body static-page}))
+
+(type (edn/read-string "{:data some}"))
